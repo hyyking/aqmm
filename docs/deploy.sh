a9869901b579f788818ebe021a858561289176c9
hyyking <leoduret@outlook.com>
2020-07-26 22:55:19 +0200

    gh-pages deploy

---
+++ docs/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "====> deploying to github"
+
+git worktree add "/tmp/book" "gh-pages"
+
+rm -rf /tmp/book/*
+cp -rp book/* /tmp/book/
+
+cd /tmp/book && \
+	git add -A && \
+	git commit -m "deployed on $(shell date) by ${USER}" && \
+	git push origin gh-pages
